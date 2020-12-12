f3543ae96bdf9954be408fe45e651cf9656efe2e
nazlikok <nazlikok@hotmail.com>
2020-12-12 13:16:39 +0300

    4.th

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,18 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File temp = new File("filename.txt");
-        try {
-            Scanner myFile = new Scanner(temp);
-            System.out.println(myFile.nextLine());
-            System.out.println(myFile.nextLine());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
+        Scanner myFile = new Scanner(temp);
+        System.out.println(myFile.nextLine());
+        System.out.println(myFile.nextLine());
 
     }
 }
