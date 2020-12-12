792ec096d04545eb607535adbae53b9d402c2052
nazlikok <nazlikok@hotmail.com>
2020-12-12 12:56:23 +0300

    1.st

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myFile = new Scanner("filename.txt");
+
+    }
+}
