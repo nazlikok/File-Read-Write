bab87e905a2b0c6d4b5a4b4759ab85e5c597a919
nazlikok <nazlikok@hotmail.com>
2020-12-12 16:12:43 +0300

    4.th

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        File temp = new File("filename.txt");
 
-        Scanner myFile = new Scanner(temp);
-        System.out.println(myFile.nextLine());
-        System.out.println(myFile.nextLine());
+    public static void main(String[] args) {
+        Files n = new Files();
+        n.openFile();
+        n.readFile();
+    }
 
+    public static class Files {
+        private Scanner myFile;
+
+        public void openFile(){
+            try {
+                myFile = new Scanner(new File("filename.txt"));
+            } catch (FileNotFoundException e) {
+                System.out.println("couldn't find the file");
+            }
+        }
+
+        public void readFile(){
+            while(myFile.hasNext()){
+                String a= myFile.next();
+                String b= myFile.next();
+
+                System.out.printf("%s %s \n",a,b);
+
+            }
+        }
     }
 }
+
+
+
